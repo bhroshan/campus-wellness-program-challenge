1e0e4875dd1215e2e0acc334d2194cd0cc3e2f79
Roshan Bhandary <bhandaryroshan6@gmail.com>
2025-06-09 09:42:56 -0600

    react frontend initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Hello MUI</h1>
+      <Button variant="contained" color="primary">
+        Campus Wellness Challenge Platform
+      </Button>
+    </div>
+  );
+}
+
+export default App;
